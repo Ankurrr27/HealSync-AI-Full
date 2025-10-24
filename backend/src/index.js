181e604a40ec53e63b2a2b27616b5fee1a28e27f
Ankur <ankurp22singh@gmail.com>
2025-10-24 13:41:49 +0530

    added patients records

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,17 +4,19 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
 import profileRoutes from "./routes/profileRoutes.js";
 import path from "path";
+import userRecordsRoutes from "./routes/userRecordsRoutes.js";
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json()); // must be BEFORE routes
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use("/api", authRoutes);
 app.use("/api/profile", profileRoutes);
+app.use("/api/records", userRecordsRoutes);
 
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
