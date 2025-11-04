38515cd8bbee389c40c1574d15a47bfdbd784fe4
Ankur <ankurp22singh@gmail.com>
2025-11-04 06:51:04 +0530

    fixed port issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,6 +34,7 @@ app.use("/api/medicines", medicineRoutes);
 
 
 // 🌍 Start server
-app.listen(5000, () =>
-  console.log("🚀 Server running on http://localhost:5000")
-);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
